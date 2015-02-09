e7e1cd76af563c2a5a0fbffcc4630895315375c4
zhangls <ihrthk@gmail.com>
2015-02-09 18:06:38 +0800

    delete hello task

--- src/main/groovy/org/gradle/GreetingPlugin.groovy
+++ src/main/groovy/org/gradle/GreetingPlugin.groovy
@@ -5,8 +5,6 @@ import org.gradle.api.Project
 
 class GreetingPlugin implements Plugin<Project> {
     void apply(Project target) {
-        target.task('hello', type: GreetingTask)
-
         target.task('mulchannel', type: MulchannelTask)
         target.extensions.create('mulchannel', ConfigExtension)
     }
