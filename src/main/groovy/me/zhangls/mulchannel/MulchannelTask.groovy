063d405954bad10b9ba753c9ebc6543c9120b8a4
ihrthk <ihrthk@gmail.com>
2015-07-14 14:05:11 +0800

    support default temp dir

--- src/main/groovy/me/zhangls/mulchannel/MulchannelTask.groovy
+++ src/main/groovy/me/zhangls/mulchannel/MulchannelTask.groovy
@@ -4,22 +4,30 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class MulchannelTask extends DefaultTask {
+
+    File DEFAULT_TEMP = new File(project.getProjectDir(), "temp")
+
     @TaskAction
     def mulchannel() {
         //1.get config extension
         ConfigExtension config = project.mulchannel
         //2.decompression zip file to tempDir
         def ant = new AntBuilder();
-        ant.unzip(src: config.inputFile, dest: config.tempDir)
+
+        def temp = config.tempDir == null ? DEFAULT_TEMP : config.tempDir;
+        ant.unzip(src: config.inputFile, dest: temp)
         //3.iterater channels
         config.channels.each { item ->
             //3.1.new a channel file
-            File file = new File(new File(config.tempDir, "META-INF"), "mulchannel_" + item)
+            File file = new File(new File(temp, "META-INF"), "mulchannel_" + item)
             file.createNewFile()
             //3.2.zip the temp dir
-            ant.zip(destfile: new File(config.outputDir, item + ".apk"), basedir: "temp");
+            ant.zip(destfile: new File(config.outputDir, item + ".apk"), basedir: temp.getName());
             //3.3.delete channel file
             file.delete()
+        }
+        if (config.tempDir == null) {
+            DEFAULT_TEMP.deleteDir();
         }
     }
 }
